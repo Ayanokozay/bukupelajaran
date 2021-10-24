d429cb9f78178c2da40184a9ed191ac1855176f6
Zay <zayvseverybody@gmail.com>
2021-10-24 19:55:53 +0700

    upload pdf ke asset

--- app/src/main/java/com/lxmedia/buton/bukupelajaran/Buku_Python_A5.java
+++ app/src/main/java/com/lxmedia/buton/bukupelajaran/Buku_Python_A5.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class Buku_Python_A5 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buku_python_a5);
+
+        PDFView pdfView = findViewById(R.id.pythona5);
+
+        pdfView.fromAsset("Buku Python A5.pdf").load();
     }
 }
