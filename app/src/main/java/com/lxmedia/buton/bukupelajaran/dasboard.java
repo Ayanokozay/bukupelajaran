28afa32f11fa2a1aeb5e286717d2fe787e46d7c5
Zay <zayvseverybody@gmail.com>
2021-10-31 22:09:20 +0700

    membuat beberapa menu

--- app/src/main/java/com/lxmedia/buton/bukupelajaran/dasboard.java
+++ app/src/main/java/com/lxmedia/buton/bukupelajaran/dasboard.java
@@ -8,18 +8,26 @@ import android.os.Bundle;
 import android.view.View;
 
 public class dasboard extends AppCompatActivity {
-    private CardView bahasapemrogramanbtn;
+    private CardView bahasapemrogramanbtn, pemdasbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dasboard);
         bahasapemrogramanbtn = findViewById(R.id.bahasapemrogramanbtn);
+        pemdasbtn = findViewById(R.id.pemdasbtn);
 
         bahasapemrogramanbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+        pemdasbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), pemdassmk.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/lxmedia/buton/bukupelajaran/pemdassmk.java
@@ -0,0 +1,14 @@
+package com.lxmedia.buton.bukupelajaran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pemdassmk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pemdassmk);
+    }
+}
